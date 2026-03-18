82dd667e21dc34faf94b925044fb1b7cd2f9e448
Jesse Ola <Jesse@Jesses-MacBook-Pro.local>
2026-03-18 17:30:29 +0000

    Added func that is shortcut for resolving the value of another service based on key

--- ioc.go
+++ ioc.go
@@ -2,8 +2,79 @@
 // Transient, Singleton and Scoped dependency lifetimes.
 package ioc
 
+import (
+	"fmt"
+	"reflect"
+)
+
 var DefaultContainer Container
+
+type Func func(Container) []reflect.Value
 
 func init() {
 	DefaultContainer = NewContainer()
+}
+
+func Service[T any]() func(Container) T {
+	return func(c Container) T {
+		return MustGet[T](c)
+	}
+}
+
+func ServiceNamed[T any](name string) func(Container) T {
+	return func(c Container) T {
+		return MustGetNamed[T](c, name)
+	}
+}
+
+func MakeFunc(fn any) (Func, error) {
+	if err := ensureFunc(fn); err != nil {
+		return nil, err
+	}
+	return func(c Container) []reflect.Value {
+		var deps []reflect.Value
+		t := reflect.TypeOf(fn)
+
+		for i := 0; i < t.NumIn(); i++ {
+			svc, err := c.Get(Key{Type: t.In(i)})
+			if err != nil {
+				panic(err)
+			}
+			deps = append(deps, reflect.ValueOf(svc))
+		}
+
+		return reflect.ValueOf(fn).Call(deps)
+	}, nil
+}
+
+func MakeFuncFromKeys(fn any, keys []Key) (Func, error) {
+	t := reflect.TypeOf(fn)
+
+	if err := ensureFunc(fn); err != nil {
+		return nil, err
+	}
+	if err := checkArgLength(t, keys); err != nil {
+		return nil, err
+	}
+
+	return func(c Container) []reflect.Value {
+		var deps []reflect.Value
+
+		for _, key := range keys {
+			svc, err := c.Get(key)
+			if err != nil {
+				panic(err)
+			}
+			deps = append(deps, reflect.ValueOf(svc))
+		}
+
+		return reflect.ValueOf(fn).Call(deps)
+	}, nil
+}
+
+func checkArgLength(fnType reflect.Type, keys []Key) error {
+	if len(keys) != fnType.NumIn() {
+		return fmt.Errorf("number of keys does not match number of function args")
+	}
+	return nil
 }
